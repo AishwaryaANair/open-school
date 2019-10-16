b1f87bdd7460dd9e2bc8d94cc9dba381288c837a
Shiv12patil <49022546+Shiv12patil@users.noreply.github.com>
2019-10-16 15:50:11 +0530

    Update urls.py

--- openSchool/school/urls.py
+++ openSchool/school/urls.py
@@ -9,16 +9,21 @@ urlpatterns = [
     path('login.html',views.loginView, name ='login'),
     path('', views.home, name='home'),  
     path('signupnext.html', views.SignUpNext, name='SignUpNext'),
-    path('courseprogress.html', views.checkProgress, name='checkProgress'),
     path('signlast.html',views.SignInLast, name='SignLast'),
     path('instructor.html', views.instructorDash, name='instructorDash'),
     path('home.html', views.logoutRequest, name='logout'),
     path('addcourse.html',views.courseAdd, name = 'addCourse'),
     path('courseedit.html?courseUID=<int:courseUID>', views.courseEdit, name = 'courseedit'),
     path('addweekcontent.html?courseUID=<int:courseUID>', views.addWeekContent, name = 'addweek'),
-    path('editweekcontent.html?weekUID=<int:weekUID>', views.editWeekContent, name = 'weekedit'),
+    path('addcontent.html?weekUID=<int:weekUID>', views.editWeekContent, name = 'weekedit'),
     path('courseview.html?courseUID=<int:courseUID>', views.courseView, name = 'viewcourse'),
     path('updatedcourse?courseID=<int:courseID>', views.updatedView, name = 'updatedCourse'),
+    path("learner.html", views.learner, name="learner"),
+    path("courseprogress.html", views.courseprogress, name='courseprogress'),
+    path("completedcourses.html", views.completedcourses, name='completedcourses'),
+    path("coursedetails.html", views.coursedetails, name='coursedetails'),
+    path("coursecontent.html", views.coursecontent, name='coursecontent'), 
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
